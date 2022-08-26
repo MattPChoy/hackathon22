e173d3502e44402a32bed3e97ba63c5b95416e1c
matthew.choy <matthew.choy@uqconnect.edu.au>
2022-08-26 22:32:22 +1000

    initial version of frontend showing placeholder logo and button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
+import coffee from "./images/coffee.png"
+import coffee_top from "./images/coffee-top.png"
+import Button from '@mui/material/Button';
 import './App.css';
+
+let loading = coffee_top
+function clearCoffee() {
+    document.getElementById("App-logo").style.display="none";
+    document.getElementById("click-here").style.display="absolute";
+}
+
+setTimeout(() => {
+    clearCoffee();
+}, 3500)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         <img src={loading} id="App-logo" className="App-logo" alt="logo" />
+         <Button style={buttonStyle} variant="outlined" id="click-here"> Click here </Button>
       </header>
     </div>
   );
+}
+
+const buttonStyle = {
+    display: 'none'
 }
 
 export default App;
