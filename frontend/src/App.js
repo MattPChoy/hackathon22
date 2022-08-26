82833670fbdec988141915d22f3b8793daf03c2b
matthew.choy <matthew.choy@uqconnect.edu.au>
2022-08-27 09:21:05 +1000

    working on breaking down existing monolithic app into smaller react components per screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,30 @@
-import logo from './logo.svg';
-import coffee from "./images/coffee.png"
-import coffee_top from "./images/coffee-top.png"
+import coffee_top from "./assets/images/coffee-top.png"
+
+import Loading from "./components/Loading"
+import Selection from "./components/Selection"
+
 import Button from '@mui/material/Button';
 import './App.css';
+import React from 'react';
 
-let loading = coffee_top
-function clearCoffee() {
-    document.getElementById("App-logo").style.display="none";
-    document.getElementById("click-here").style.display="absolute";
-}
+// Loading icon
+// function clearCoffee() {
+//     document.getElementById("App-logo").style.display="none";
+//     document.getElementById("click-here").style.display="block";
+// }
 
-setTimeout(() => {
-    clearCoffee();
-}, 3500)
+// setTimeout(() => {
+//     clearCoffee();
+// }, 3500)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-         <img src={loading} id="App-logo" className="App-logo" alt="logo" />
-         <Button style={buttonStyle} variant="outlined" id="click-here"> Click here </Button>
+         <Selection />
       </header>
     </div>
   );
-}
-
-const buttonStyle = {
-    display: 'none'
 }
 
 export default App;
