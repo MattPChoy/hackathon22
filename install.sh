b362e20dab8c64a8eae77d283c29be7226184fb3
CaptianDynamite <kalebvanm@gmail.com>
2022-08-27 19:37:00 +1000

    Added bash script to install the server and its dependencies and create the .env file.

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+if [ $# -lt 1 ]
+then
+  echo "Too few arguments provided. First argument must be the mongodb database link."
+  echo "Run this script with the argument help for more information"
+  exit 1
+fi
+if [ "$1" = "help" ]
+then
+  echo "The first argument must be the mongodb database link."
+  echo "The optional second parameter is \"test\" which will enable the /test_api/ series of API endpoints."
+  exit 0
+fi
+
+npm i || exit
+cd ./frontend || exit
+npm i || exit
+npm run build || exit
+cd ../
+touch .env
+echo "MONGO_API_KEY=$1" > .env
+
+if [ $# -eq 2 ]
+then
+  echo "TEST_API=1" >> .env
+fi
+
+echo "Server successfully initialised, run 'node index.js listen <port_number>' to start the webserver!"
